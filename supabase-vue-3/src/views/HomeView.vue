85dd424e951ac0f0757391e26c8e72be656b4d96
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-05-09 13:56:13 -0400

    1

--- supabase-vue-3/src/views/HomeView.vue
+++ supabase-vue-3/src/views/HomeView.vue
@@ -1,5 +1,25 @@
 <script setup>
+import {supabase} from '../supabase'
 
+const loading = ref(false)
+const email = ref('')
+
+const handleLogin = async () => {
+  try {
+    loading.value = true
+    const { error } = await supabase.auth.signInWithOtp({
+      email: email.value,
+    })
+    if (error) throw error
+    alert('Check your email for the login link!')
+  } catch (error) {
+    if (error instanceof Error) {
+      alert(error.message)
+    }
+  } finally {
+    loading.value = false
+  }
+}
 </script>
 
 <template>
