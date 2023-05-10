da9bca9d19548c3be9872a21e9d5cb1b4e221340
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-05-10 13:30:32 -0400

    1

--- supabase-vue-3/src/supabase.js
+++ supabase-vue-3/src/supabase.js
@@ -1,6 +1,6 @@
-
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://cjyttlzxoulnklcsicjs.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqeXR0bHp4b3VsbmtsY3NpY2pzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2NTI4ODksImV4cCI6MTk5OTIyODg4OX0.FgDPsmtpD3qBOP8-lXKL7KVdqVGnJkRqs7VfWG0xjmY'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
--- supabase-vue-3/src/views/HomeView.vue
+++ supabase-vue-3/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {supabase} from '../supabase'
+import { ref } from 'vue'
+import { supabase } from '../supabase'
 
 const loading = ref(false)
 const email = ref('')
@@ -23,5 +24,21 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <h1>hi</h1>
+  <form class="row flex-center flex" @submit.prevent="handleLogin">
+    <div class="col-6 form-widget">
+      <h1 class="header">Supabase + Vue 3</h1>
+      <p class="description">Sign in via magic link with your email below</p>
+      <div>
+        <input class="inputField" required type="email" placeholder="Your email" v-model="email" />
+      </div>
+      <div>
+        <input
+          type="submit"
+          class="button block"
+          :value="loading ? 'Loading' : 'Send magic link'"
+          :disabled="loading"
+        />
+      </div>
+    </div>
+  </form>
 </template>
