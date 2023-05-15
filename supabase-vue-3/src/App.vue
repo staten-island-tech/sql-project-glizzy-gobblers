abcf7333d2cbab2b2bf204db8b020912e11945c6
AndrewL777 <7landrew7@gmail.com>
2023-05-15 13:59:31 -0400

    234234

--- supabase-vue-3/src/App.vue
+++ supabase-vue-3/src/App.vue
@@ -4,78 +4,11 @@ import datapage from './views/datapage.vue'
 </script>
 
 <template>
-  <main>
-    <header>
-      <div class="wrapper">
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
-      </div>
-    </header>
-    <RouterView />
-  </main>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </header>
+  <RouterView />
 </template>
 
-<style scoped>
-main {
-  background-image: url('https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000');
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
--- supabase-vue-3/src/views/HomeView.vue
+++ supabase-vue-3/src/views/HomeView.vue
@@ -1,49 +1,17 @@
-<script setup>
-import { ref } from 'vue'
-import { supabase } from '../supabase'
+<script setup></script>
 
-const loading = ref(false)
-const email = ref('')
-
-const handleLogin = async () => {
-  try {
-    loading.value = true
-    const { error } = await supabase.auth.signInWithOtp({
-      email: email.value
-    })
-    if (error) throw error
-    alert('Check your email for the login link!')
-  } catch (error) {
-    if (error instanceof Error) {
-      alert(error.message)
-    }
-  } finally {
-    loading.value = false
-  }
-}
-</script>
-
-<template img>
-  <form class="row flex-center flex" @submit.prevent="handleLogin">
-    <div class="col-6 form-widget">
-      <h1 class="header">Welcome to _ Blogpost!</h1>
-      <p class="description">
-        Here you can share your experiences and ideas with people around the globe!
-      </p>
-      <div>
-        <input class="inputField" required type="email" placeholder="Your email" v-model="email" />
-      </div>
-      <div>
-        <input
-          type="submit"
-          class="button block"
-          :value="loading ? 'Loading' : 'Send magic link'"
-          :disabled="loading"
-        />
-      </div>
-    </div>
-  </form>
+<template>
+  <div>
+    <h1>dsjdssd</h1>
+  </div>
 </template>
+
+<style scoped>
+div {
+  background-color: rgb(248, 200, 220);
+  padding-bottom: 1000px;
+}
+</style>
 <!-- <style>
 template {
   background-image: url('https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000');
