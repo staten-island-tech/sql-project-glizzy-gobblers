b94e65ab9b89e32340b5d884cc1321b4f3d51faa
AndrewL777 <7landrew7@gmail.com>
2023-05-16 14:00:05 -0400

    Update HomeView.vue

--- supabase-vue-3/src/views/HomeView.vue
+++ supabase-vue-3/src/views/HomeView.vue
@@ -2,14 +2,29 @@
 
 <template>
   <div>
-    <h1>dsjdssd</h1>
+    <h1>I will never take my glorious king for granted🥰🐐👑</h1>
+    <img
+      src="https://i.ytimg.com/vi/BZCB8yMSkWI/maxresdefault.jpg"
+      alt="My Glorious King"
+      class="center"
+    />
   </div>
 </template>
 
 <style scoped>
+h1 {
+  color: brown;
+  text-align: center;
+}
 div {
   background-color: rgb(248, 200, 220);
-  padding-bottom: 1000px;
+  padding-bottom: 600px;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
 }
 </style>
 <!-- <style>
