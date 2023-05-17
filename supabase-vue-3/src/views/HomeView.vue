e787831f7d7acb95ac6a088e816656b64766b2d2
KevinZheng29 <113195602+KevinZheng29@users.noreply.github.com>
2023-05-17 13:56:36 -0400

    1

--- supabase-vue-3/src/views/HomeView.vue
+++ supabase-vue-3/src/views/HomeView.vue
@@ -1,34 +1,58 @@
-<script setup></script>
+<script>
+import { ref } from 'vue';
+import loginpage from './loginpage.vue'
+
+export default{
+  data(){
+    return{
+      page: 'homepage'
+    }
+  },
+  methods:{
+    loginpage: function(){
+      this.page = 'loginpage'
+    },
+    createaccount: function(){
+      this.page = 'createaccount'
+    }
+  },
+  components:{
+    loginpage
+  }
+}
+</script>
 
 <template>
-  <div>
-    <h1>I will never take my glorious king for granted🥰🐐👑</h1>
-    <img
-      src="https://i.ytimg.com/vi/BZCB8yMSkWI/maxresdefault.jpg"
-      alt="My Glorious King"
-      class="center"
-    />
+
+    <div class="homepage" v-if="page === 'homepage'">
+    <h1 class="title">TechBlog</h1>
+    <div class="btnpage">
+      <button @click="loginpage" class="btn">Login</button>
+      <button class="btn">Create Account</button>
+    </div>
   </div>
+
+    <div v-if="page === 'loginpage'">
+        <loginpage/>
+    </div>
+
 </template>
 
 <style scoped>
-h1 {
-  color: brown;
+
+.title{
+  font-size: 100px;
   text-align: center;
 }
-div {
-  background-color: rgb(248, 200, 220);
-  padding-bottom: 600px;
+
+.btnpage{
+  margin-top: 100px;
+  text-align: center;
 }
-.center {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
+.btn{
+  font-size: 50px;
+  background-color: transparent;
+  margin-left: 50px;
+  margin-right: 50px;
 }
-</style>
-<!-- <style>
-template {
-  background-image: url('https://img.freepik.com/premium-photo/image-colorful-galaxy-sky-generative-ai_791316-9864.jpg?w=2000');
-}
-</style> -->
+</style> 
