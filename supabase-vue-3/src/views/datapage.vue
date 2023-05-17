5a0c8b4dc1b20a9f0430a9e73220a84cde99346d
AndrewL777 <7landrew7@gmail.com>
2023-05-17 13:37:24 -0400

    Update datapage.vue

--- supabase-vue-3/src/views/datapage.vue
+++ supabase-vue-3/src/views/datapage.vue
@@ -1,6 +1,5 @@
 <template>
-
-<!-- <div class="blogpage">
+  <!-- <div class="blogpage">
 
   <div class="postbox">
 
@@ -20,67 +19,76 @@
   </div>
 
 </div> -->
-
- <div v-for="data in database">
-     <div class="blogpage">
-
-  <div class="postbox">
-
-  <h1 class="createdby">{{ data.createdby }}</h1>
-        <div class="postimage">
-           <img class="img" :src="data.imagelink">
+  <body>
+    <div v-for="data in database">
+      <div class="blogpage">
+        <div class="postbox">
+          <h1 class="createdby">{{ data.createdby }}</h1>
+          <div class="postimage">
+            <img class="img" :src="data.imagelink" />
+          </div>
+          <div class="titlepage">
+            <h1 class="title">{{ data.title }}</h1>
+          </div>
+          <div class="descriptionpage">
+            <h1 class="postdescription">
+              {{ data.description }}
+            </h1>
+            <img class="heart" src="https://i.stack.imgur.com/Ui4gd.png" alt="" />
+          </div>
         </div>
-        <div class="titlepage">
-          <h1 class="title">{{ data.title }}</h1>
-        </div>
-  <div class="descriptionpage">
-       <h1 class="postdescription">
-        {{ data.description }}
-       </h1>
-  </div>
-
-  </div>
-
-</div>
-</div>
-
+      </div>
+    </div>
+  </body>
 </template>
 
 <style scoped>
+h1 {
+  color: brown;
+  text-align: center;
+}
+body {
+  background-color: rgb(248, 200, 220);
+}
+.heart {
+  margin-left: 10px;
 
-.blogpage{
-  margin-left: 40px;
+  height: 35px;
 }
-.postbox{
+.blogpage {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.postbox {
+  background-color: white;
   width: 500px;
-  height: 700px;
+  height: 600px;
   border: 3px black solid;
-  margin-bottom: 20px;
+  margin-bottom: 25px;
 }
-.createdby{
+.createdby {
   margin-left: 10px;
   font-size: 20px;
 }
 
-.postimage{
+.postimage {
   display: flex;
   justify-content: center;
 }
-.img{
+.img {
   width: 480px;
   height: 400px;
   object-fit: cover;
 }
 
-.title{
+.title {
   font-size: 30px;
 }
-.postdescription{
+.postdescription {
   font-size: 20px;
 }
-
 </style>
-
 
 <script setup>
 import { ref, onMounted } from 'vue'
@@ -88,15 +96,13 @@ import { supabase } from '../supabase'
 
 let database = ref('')
 
-async function getdata(){
-  const { data, error } = await supabase
-  .from('userpost')
-  .select()
-console.log(data)
-database.value = data
+async function getdata() {
+  const { data, error } = await supabase.from('userpost').select()
+  console.log(data)
+  database.value = data
 }
 
 onMounted(() => {
-getdata()
+  getdata()
 })
 </script>
